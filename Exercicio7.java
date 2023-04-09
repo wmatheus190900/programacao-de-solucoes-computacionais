ec38b29699f21fd8a6ccaf4c04c8b8fa08d3eaf3
Wanderson Matheus <w.matheuspontes2000@gmail.com>
2023-04-09 12:27:37 -0300

    7

---
+++ Exercicio7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Exercicio7 {
+    public static void main(String[] args) {
+		double salmin,salf,quantsal;
+		Scanner bot= new Scanner(System.in);
+		System.out.print("valor do salário mínimo:");
+		salmin=bot.nextDouble();
+		System.out.print("quanto vc ganha?");
+		salf=bot.nextDouble();
+		quantsal=salf/salmin;
+		System.out.print("vc recebe "+quantsal+" minimos");
+		bot.close();
+	}
+    
+}
