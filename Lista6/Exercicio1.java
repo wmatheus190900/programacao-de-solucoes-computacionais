5601b1430daa567383ca22758aedeafe4d528649
Wanderson Matheus <w.matheuspontes2000@gmail.com>
2023-05-04 12:13:41 -0300

    novo commit

---
+++ Lista6/Exercicio1.java
@@ -0,0 +1,22 @@
+package Lista6;
+import java.util.Scanner;
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner bot=new Scanner(System.in);
+        int i,somaPares=0;
+        int[] inteiros = new int[10];
+        for(i=0;i<inteiros.length;i++){
+            System.out.print("Digite um número inteiro:");
+            inteiros[i]=bot.nextInt();
+            if(inteiros[i]%2==0){
+                somaPares+=inteiros[i];
+            }
+        }
+        System.out.println("Vetor informado:");
+        for (i = 0; i <inteiros.length; i++)
+        System.out.printf("v[%d] = %2d\n", i, inteiros[i]);
+        System.out.println("soma dos numeros pares: " + somaPares);
+        bot.close();
+    }
+}
+
