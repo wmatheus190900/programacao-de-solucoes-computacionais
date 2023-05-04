952902b30cf457bfb8607cdef1eeb96347248155
Wanderson Matheus <w.matheuspontes2000@gmail.com>
2023-05-03 21:51:59 -0300

    novo commit

---
+++ Lista5/Exercicio2.java
@@ -0,0 +1,16 @@
+package Lista5;
+import java.util.Scanner;
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner bot= new Scanner(System.in);
+        int primeiro,limite;
+        System.out.println("escolha 2 numeros");
+        primeiro=bot.nextInt();
+        limite=bot.nextInt();
+
+        for(int i=primeiro;i<limite;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
