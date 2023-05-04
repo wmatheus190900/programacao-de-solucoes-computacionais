bcb2c757a9b902c6226d51f825cd8d124c2fe221
Wanderson Matheus <w.matheuspontes2000@gmail.com>
2023-05-03 21:45:40 -0300

    novo commit

---
+++ Lista5/Exercicio1.java
@@ -0,0 +1,12 @@
+package Lista5;
+public class Exercicio1 {
+    public static void main(String[] args) {
+        int cont=0;
+        
+        for(int i=7;i<=20;i++){
+            cont+=i;
+        }
+        System.out.println(" a soma de 7 a 20 é " + cont);
+    }
+    
+}
