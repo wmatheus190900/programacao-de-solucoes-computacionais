6a02dd531ac856f53985f178e719ecc726c31d4a
Wanderson Matheus <w.matheuspontes2000@gmail.com>
2023-05-04 08:40:33 -0300

    novo commit

---
+++ Lista5/Exercicio6.java
@@ -0,0 +1,19 @@
+package Lista5;
+import java.util.Scanner;
+public class Exercicio6 {
+    public static void main(String[] args) {
+        Scanner bot=new Scanner(System.in);
+        int idade;
+        double media;
+        int somaIdade=0;
+        int numPessoas=0;
+        for(int i=0;i<10;i++){
+            System.out.println("Digite a idade");
+            idade=bot.nextInt();
+            somaIdade+=idade;
+            numPessoas++;
+        }
+        media=somaIdade/numPessoas;
+        System.out.println("media de idades: " + media);
+    }
+}
